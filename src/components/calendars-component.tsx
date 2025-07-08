c1eac053cb86060e605f7b380276aa23ec10a332
Amine HERRADI <amineherradi@gmail.com>
2025-07-08 02:38:05 +0200

    Feature - #NOID - Page Header Component

--- src/components/calendars-component.tsx
+++ src/components/calendars-component.tsx
@@ -1,20 +1,9 @@
-import {Button} from "@/components/ui/button";
-import {Calendar, Plus, Settings, Users} from "lucide-react";
+import {Calendar, Settings, Users} from "lucide-react";
 import {Card, CardContent} from "@/components/ui/card";
 
 export default function CalendarsComponent() {
     return (
         <>
-            <div className="flex items-center justify-between">
-                <div>
-                    <h1 className="text-3xl font-bold">My calendars</h1>
-                    <p className="text-muted-foreground">Manage calendars and organize events</p>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
-                    <Button asChild><a href="/calendars/new"><Plus className="h-4 w-4 mr-2"/>New calendar</a></Button>
-                </div>
-            </div>
-
             {/* Statistiques */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <Card>
