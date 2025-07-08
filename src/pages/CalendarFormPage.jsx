e4d16e23bdaad800813cd26a31f0bd3b80b7f6be
Amine HERRADI <amineherradi@gmail.com>
2025-07-08 05:57:26 +0200

    Fix - #NOID - Form Header Component

--- src/pages/CalendarFormPage.jsx
+++ src/pages/CalendarFormPage.jsx
@@ -1,9 +1,23 @@
 import CalendarFormComponent from "@/components/calendar-form-component.js";
 import '@/styles/CalendarFormPage.css';
+import FormHeaderComponent from "@/components/form-header-component.js";
+import {ArrowLeft} from "lucide-react";
+import * as React from "react";
 
 function CalendarFormPage() {
     return (
-        <CalendarFormComponent/>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <FormHeaderComponent
+                title={"New calendar"}
+                description={"Create or edit a shared calendar with options for title, team members, availability, and color codes."}
+                button={{
+                    name: "Back",
+                    link: "/calendars",
+                    icon: ArrowLeft
+                }}
+            />
+            <CalendarFormComponent/>
+        </div>
     );
 }
 
