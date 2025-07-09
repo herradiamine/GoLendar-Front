bfd8c206f32ac1219dc6df5fa36f07eab97d6d0c
Amine HERRADI <amineherradi@gmail.com>
2025-07-09 05:19:10 +0200

    Feature - #NOID - Return to next js finaly, yes I know...

--- src/constants/messages.js
+++
@@ -1,7 +0,0 @@
-export const ERROR_MESSAGES = {
-    login: 'Erreur de connexion',
-    refresh: 'Erreur de rafraîchissement du token',
-    logout: 'Erreur de déconnexion',
-    profile: 'Impossible de récupérer le profil',
-    technical: 'Erreur technique : réponse du serveur illisible.'
-}; 
