0a4bdff9f468e8049c2d410c683ab67067255d3e
Amine HERRADI <amineherradi@gmail.com>
2025-07-14 02:42:02 +0200

    Feature - #NOID - Add events and calendars pages

---
+++ src/app/calendars/[calendar_id]/events/[event_id]/page.tsx
@@ -0,0 +1,19 @@
+import PageHeaderComponent from "@/components/page-header";
+
+export default function Page() {
+    return (
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <PageHeaderComponent
+                title={"Get Event"}
+                description={"Here is the event you wanted to see."}
+                buttons={[]}
+            />
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl"/>
+                <div className="bg-muted/50 aspect-video rounded-xl"/>
+                <div className="bg-muted/50 aspect-video rounded-xl"/>
+            </div>
+            <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"/>
+        </div>
+    )
+}
